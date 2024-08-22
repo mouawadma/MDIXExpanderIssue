d9dc15c099d77595beb967ce2bdda58cb847598e
mouawadm <mouawadm@GBN203>
2024-08-22 09:53:28 -0400

    Add project files.

---
+++ TestingMDIX51Local/Window1.xaml.cs
@@ -0,0 +1,42 @@
+﻿using MaterialDesignColors;
+using MaterialDesignThemes.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TestingMDIX51Local
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeMaterialDesign();
+            InitializeComponent();
+            var mdixWindowStyle = this.FindResource("MaterialDesignWindow") as Style;
+            this.Style = mdixWindowStyle;
+        }
+
+        private void InitializeMaterialDesign()
+        {
+            // Create dummy objects to force the MaterialDesign assemblies to be loaded
+            // from this assembly, which causes the MaterialDesign assemblies to be searched
+            // relative to this assembly's path. Otherwise, the MaterialDesign assemblies
+            // are searched relative to Eclipse's path, so they're not found.
+            var card = new Card();
+            var hue = new Hue("Dummy", Colors.Black, Colors.White);
+        }
+    }
+}
